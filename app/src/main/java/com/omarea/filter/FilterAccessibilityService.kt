6d08d8910b4f9bc953b97af425960873891ceadc
kuanglinfei <kuanglinfei@phone580.com>
2019-04-26 18:40:04 +0800

    更新 配置

--- app/src/main/java/com/omarea/filter/FilterAccessibilityService.kt
+++ app/src/main/java/com/omarea/filter/FilterAccessibilityService.kt
@@ -14,6 +14,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Handler
 import android.provider.Settings
+import android.util.Log
 import android.view.*
 import android.view.accessibility.AccessibilityEvent
 import android.widget.Toast
