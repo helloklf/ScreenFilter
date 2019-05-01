942726cd9ada642b97e25e6fbdad0641ab41f1dd
kuanglinfei <helloklf@outlook.com>
2019-05-01 15:52:56 +0800

    Revert "更新 配置"
    This reverts commit 6d08d8910b4f9bc953b97af425960873891ceadc.

--- app/src/main/java/com/omarea/filter/FilterAccessibilityService.kt
+++ app/src/main/java/com/omarea/filter/FilterAccessibilityService.kt
@@ -14,7 +14,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Handler
 import android.provider.Settings
-import android.util.Log
 import android.view.*
 import android.view.accessibility.AccessibilityEvent
 import android.widget.Toast
